34ec06ae83678e64c5ba1f1ddcd4cd1653a000a8
Isuru Ranasinghe <isuru@jrcsoftware.com>
2020-02-06 16:54:12 +0530

    Initial commit

---
+++ BricksAndAgentPods/BricksAndAgentPods.h
@@ -0,0 +1,19 @@
+//
+//  BricksAndAgentPods.h
+//  BricksAndAgentPods
+//
+//  Created by Isuru Ranasinghe on 2/6/20.
+//  Copyright © 2020 Isuru Ranasinghe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BricksAndAgentPods.
+FOUNDATION_EXPORT double BricksAndAgentPodsVersionNumber;
+
+//! Project version string for BricksAndAgentPods.
+FOUNDATION_EXPORT const unsigned char BricksAndAgentPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BricksAndAgentPods/PublicHeader.h>
+
+
